d92a2cd62b57f0e01723463b27820a99961c6c2f
MrHeadbang <63957829+MrHeadbang@users.noreply.github.com>
2022-01-24 00:50:47 +0100

    Update DiceBot.py

--- Discord/DiceBot.py
+++ Discord/DiceBot.py
@@ -26,18 +26,20 @@ class Client(discord.Client):
         global lastCommand
         if message.author == client.user:
             return
-        try:
-            s = str(message.content)
-            if s == "r":
-                s = lastCommand
-            n = int(s.split("x")[0])
-            d = int(s.split("x")[1])
-            lastCommand = s
-            await dice(n, d, message)
-        except:
-            await message.channel.send("Falsches Format. Schreibe 'help' für Hilfe.")
-        if message.content == "help":
-            await message.channel.send("Würfel Format: [Anzahl an Würfeln]x[Seitenanzahl der Würfel]\nBeispiele:\n3x6 (Würfelt 3mal einen 6seitigen Würfel)\n2x10 (Würfelt 2mal einen 10seitigen Würfel)\n\nSchreibe r, um den letzten Befehl zu widerholen.")
-
+        if message.content == "!d help":
+                await message.channel.send("Würfel Format: !d [Anzahl an Würfeln]x[Seitenanzahl der Würfel]\nBeispiele:\n3x6 (Würfelt 3mal einen 6seitigen Würfel)\n2x10 (Würfelt 2mal einen 10seitigen Würfel)\n\nSchreibe r, um den letzten Befehl zu widerholen.")
+                return
+        if message.content.startswith("!d"):
+            try:
+                s = str(message.content).replace("!d ", "").replace(" ", "")
+                if s == "r":
+                    s = lastCommand
+                n = int(s.split("x")[0])
+                d = int(s.split("x")[1])
+                lastCommand = s
+                await dice(n, d, message)
+            except:
+                await message.channel.send("Falsches Format. Schreibe '!d help' für Hilfe.")
+            
 client = Client()
 client.run("YOUR TOKEN")
